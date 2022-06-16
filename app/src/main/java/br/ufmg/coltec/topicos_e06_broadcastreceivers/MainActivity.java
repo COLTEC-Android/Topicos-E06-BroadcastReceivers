da4e4be831a1534bd6f1926fc22fca13bd5397ea
coltec <coltec@coltec.com>
2022-06-15 21:02:30 -0300

    Tópicos Android E06: Broadcast Receivers

--- app/src/main/java/br/ufmg/coltec/topicos_e06_broadcastreceivers/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/topicos_e06_broadcastreceivers/MainActivity.java
@@ -2,14 +2,31 @@ package br.ufmg.coltec.topicos_e06_broadcastreceivers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static int CURRENT_THEME = R.style.Theme_TopicosE06BroadcastReceivers;
 
-    // TODO: Criar os receivers para tratar a mudança do tema
+    private BroadcastReceiver batteryLow = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            switchActivityTheme(R.style.Theme_TopicosE06BroadcastReceiversLowBattery);
+
+        }
+    };
+
+    private BroadcastReceiver batteryOk = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            switchActivityTheme(R.style.Theme_TopicosE06BroadcastReceivers);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +34,17 @@ public class MainActivity extends AppCompatActivity {
         this.setTheme(CURRENT_THEME);
         setContentView(R.layout.activity_main);
 
-        // TODO: registrar os receivers
+        IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_LOW);
+        this.registerReceiver(this.batteryLow, filter);
+        IntentFilter filterOK = new IntentFilter(Intent.ACTION_BATTERY_OKAY);
+        this.registerReceiver(this.batteryOk, filterOK);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
-        // TODO: desregistrar os receivers
+        unregisterReceiver(this.batteryLow);
+        unregisterReceiver(this.batteryOk);
     }
 
     /**
